dfb513423037aebfa103389a0f9a4d429131fae2
IGOR_DA_SILVA_ARAUJO <IGOR_DA_SILVA_ARAUJO>
2018-02-19 22:37:09 -0300

    commit back-end - aula01

--- aula01/app/src/main/java/br/com/cotemig/aula01/MainActivity.java
+++ aula01/app/src/main/java/br/com/cotemig/aula01/MainActivity.java
@@ -2,12 +2,49 @@ package br.com.cotemig.aula01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView btnIgual;
+    Button btn01;
+    Button btn02;
+    Button btn03;
+    Button btn04;
+    Button btn05;
+    Button btn06;
+    Button btn07;
+    Button btn08;
+    Button btn09;
+    Button btn00;
+    Button btnPonto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIgual = findViewById(R.id.btnIgual);
+        btn01 = findViewById(R.id.btn01);
+
+        btn01.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()) {
+            case 0:
+                btnIgual.setText("0");
+                break;
+            case 1:
+
+                break;
+            default:
+
+        }
+
     }
 }
