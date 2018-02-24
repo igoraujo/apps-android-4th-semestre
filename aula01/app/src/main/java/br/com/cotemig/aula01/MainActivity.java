aa95f144195b9e2a6447dad81df3bfa0aae505fb
IGOR_DA_SILVA_ARAUJO <IGOR_DA_SILVA_ARAUJO>
2018-02-23 22:06:19 -0300

    incluindoa biblioteca BUTTER KNIFE

--- aula01/app/src/main/java/br/com/cotemig/aula01/MainActivity.java
+++ aula01/app/src/main/java/br/com/cotemig/aula01/MainActivity.java
@@ -6,45 +6,72 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import butterknife.BindView;
+import butterknife.OnClick;
 
-    TextView btnIgual;
-    Button btn01;
-    Button btn02;
-    Button btn03;
-    Button btn04;
-    Button btn05;
-    Button btn06;
-    Button btn07;
-    Button btn08;
-    Button btn09;
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.txtResultado)
+    TextView txtResultado;
+
+    @BindView(R.id.btn00)
     Button btn00;
-    Button btnPonto;
+
+    @BindView(R.id.btn01)
+    Button btn01;
+
+    @BindView(R.id.btn02)
+    Button btn02;
+
+    @BindView(R.id.btn03)
+    Button btn03;
+
+    @BindView(R.id.btn04)
+    Button btn04;
+
+    @BindView(R.id.btn05)
+    Button btn05;
+
+    @BindView(R.id.btn06)
+    Button btn06;
+
+    @BindView(R.id.btn07)
+    Button btn07;
+
+    @BindView(R.id.btn08)
+    Button btn08;
+
+    @BindView(R.id.btn09)
+    Button btn09;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnIgual = findViewById(R.id.btnIgual);
-        btn01 = findViewById(R.id.btn01);
-
-        btn01.setOnClickListener(this);
+//        btnIgual = findViewById(R.id.btnIgual);
+//        btn01 = findViewById(R.id.btn01);
+//
+//        btn01.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-
-        switch(v.getId()) {
-            case 0:
-                btnIgual.setText("0");
-                break;
-            case 1:
-
-                break;
-            default:
-
-        }
-
+   @OnClick(R.id.btn00)
+    public  void btn00Click(){
+        txtResultado.setText('0');
     }
+
+//    public void onClick(View v) {
+//
+//        switch(v.getId()) {
+//            case 0:
+//                btnIgual.setText("0");
+//                break;
+//            case 1:
+//
+//                break;
+//            default:
+//
+//        }
+//
+//    }
 }
