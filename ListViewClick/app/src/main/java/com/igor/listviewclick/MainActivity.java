693e75515061bed3f018b935b7e13fe6f9e74024
Igor Araujo <igor.araujo@fourtime.com>
2018-03-13 18:42:17 -0300

    parte do lista e sub-lista

--- ListViewClick/app/src/main/java/com/igor/listviewclick/MainActivity.java
+++ ListViewClick/app/src/main/java/com/igor/listviewclick/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.lista)
     ListView lista;
+    ListView listaCidade;
 
     List<String> estados;
 
@@ -27,40 +28,40 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         estados = new ArrayList<>();
-        estados.add("AC");
-        estados.add("AM");
-        estados.add("AL");
-        estados.add("BA");
-        estados.add("MG");
-        estados.add("RJ");
-        estados.add("SP");
-        estados.add("BA");
-        estados.add("MG");
-        estados.add("RJ");
-        estados.add("SP");
-        estados.add("BA");
-        estados.add("MG");
-        estados.add("RJ");
-        estados.add("SP");
-        estados.add("BA");
-        estados.add("MG");
-        estados.add("RJ");
-        estados.add("SP");
-        estados.add("BA");
-        estados.add("MG");
-        estados.add("RJ");
-        estados.add("SP");
-        estados.add("BA");
-        estados.add("MG");
-        estados.add("RJ");
-        estados.add("SP");
-        estados.add("BA");
-        estados.add("MG");
-        estados.add("RJ");
-        estados.add("SP");
+
+        estados.add(1,"AC");
+        estados.add(2,"AL");
+        estados.add(3,"AM");
+        estados.add(4,"AP");
+        estados.add(5,"BA");
+        estados.add(6,"CE");
+        estados.add(27,"DF");
+        estados.add(7,"ES");
+        estados.add(8,"GO");
+        estados.add(9,"MA");
+        estados.add(10,"MG");
+        estados.add(11,"MS");
+        estados.add(12,"MT");
+        estados.add(13,"PA");
+        estados.add(14,"PB");
+        estados.add(15,"PE");
+        estados.add(16,"PI");
+        estados.add(17,"PR");
+        estados.add(18,"RJ");
+        estados.add(19,"RN");
+        estados.add(20,"RO");
+        estados.add(21,"RR");
+        estados.add(22,"RS");
+        estados.add(23,"SC");
+        estados.add(24,"SE");
+        estados.add(25,"SP");
+        estados.add(26,"TO");
 
         EstadoAdapter estadoAdapter = new EstadoAdapter(this, estados);
         lista.setAdapter(estadoAdapter);
+
+
+
 
     }
 }
