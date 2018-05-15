7ae7c140f4939a979b411510f8ab6b636f630514
Igor Araujo <igoraujo93@gmail.com>
2018-05-14 22:25:33 -0300

    add Fragments no app EAT

---
+++ Eat/app/src/main/java/br/com/cotemig/eat/ui/fragments/DetailFragment.java
@@ -0,0 +1,30 @@
+package br.com.cotemig.eat.ui.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.cotemig.eat.R;
+
+public class DetailFragment extends Fragment {
+
+    public static SearchFragment newInstance(String param1, String param2) {
+        SearchFragment fragment = new SearchFragment();
+        Bundle args = new Bundle();
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+
+}
